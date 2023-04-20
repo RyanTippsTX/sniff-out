d315391e9122be779d8f49e116b178bb5520057b
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2023-04-19 23:28:39 -0500

    style footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,67 +1,44 @@
 import Link from 'next/link';
-import { Brand } from './Brand';
+import { BrandLink } from './Brand';
+import { title } from '~/global';
 
 // list paths you want to show up in the Site Map in the site-wide footer
-// currently, some paths included in SiteMap for dev convience only
-const sitePaths = new Map<string, string>([
-  ['Home', '/'],
-  ['Demo', '/1234567/Dunder-Mifflin-Christmas-Party'], // omit in production
-  ['About', '/about'],
-  ['Statistics', '/stats'],
-]);
-const hostPaths = new Map<string, string>([
-  ['Host Login', '/login'],
-  ['My Events', '/my-events'],
-  ['New Event', '/new'], // omit in productin
-]);
-const legalPaths = new Map<string, string>([
-  ['Terms of use', '/legal/'],
-  ['Privacy policy', '/legal/'],
-  ['Cookie policy', '/legal/'],
-]);
-
-const Links = ({ routes }: { routes: Map<string, string> }) => {
-  const navElements: React.ReactElement[] = [];
-  routes.forEach((path, title) => {
-    navElements.push(
-      <div key={title}>
-        <Link className='link link-hover' href={path}>
-          {title}
-        </Link>
-      </div>
-    );
-  });
-  return <>{navElements}</>;
-};
+const footerLinks = [
+  { label: 'Home', href: '/' },
+  { label: 'About', href: '/about' },
+  { label: 'Pups', href: '/pups' },
+  { label: 'Owners', href: '/owners' },
+  { label: 'Terms', href: '/legal' },
+  { label: 'Privacy', href: '/legal' },
+  { label: 'Cookies', href: '/legal' },
+];
 
 const company = (
-  <div className='place-items-center sm:place-items-start'>
-    <Brand />
-    <p>Copyright © 2022 - All rights reserved</p>
+  <div className='flex flex-col place-items-center gap-y-1'>
+    <p className='text-xs font-thin text-zinc-300'>Copyright © 2023 {title}</p>
+    <BrandLink />
+  </div>
+);
+
+const links = (
+  <div className='flex flex-row flex-wrap gap-x-2'>
+    {footerLinks.map(({ label, href }) => (
+      <div key={label + href}>
+        <Link className='text-sm hover:underline' href={href}>
+          {label}
+        </Link>
+      </div>
+    ))}
   </div>
 );
 
 export function Footer() {
   return (
-    <div className='bg-dark text-zinc-300'>
+    <div className=''>
       <div className='container mx-auto'>
-        <div className=' flex flex-col-reverse gap-10 px-6 py-10 sm:flex-row '>
-          <div className='footer place-items-center sm:place-items-start '>{company}</div>
-
-          <div className='grid min-w-max  grid-cols-3 place-content-between gap-4  text-sm leading-7 sm:place-content-center'>
-            <div className='min-w-max'>
-              <span className='footer-title'>Site</span>
-              <Links routes={sitePaths} />
-            </div>
-            <div className='min-w-max'>
-              <span className='footer-title'>Hosts</span>
-              <Links routes={hostPaths} />
-            </div>
-            <div className='min-w-max'>
-              <span className='footer-title'>Legal</span>
-              <Links routes={legalPaths} />
-            </div>
-          </div>
+        <div className='flex flex-col place-content-center items-center gap-4 px-6 py-10'>
+          {links}
+          {company}
         </div>
       </div>
     </div>
