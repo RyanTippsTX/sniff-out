8328a210211234d7b831cac5f5b31f63deceada6
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2023-04-19 21:09:23 -0500

    delete example router and add dogs router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
 import { createTRPCRouter } from '~/server/api/trpc';
-import { exampleRouter } from '~/server/api/routers/example';
+import { dogsRouter } from '~/server/api/routers/dogs';
 
 /**
  * This is the primary router for your server.
@@ -7,7 +7,7 @@ import { exampleRouter } from '~/server/api/routers/example';
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  dogs: dogsRouter,
 });
 
 // export type definition of API
--- src/server/api/routers/dogs.ts
+++ src/server/api/routers/dogs.ts
@@ -2,13 +2,13 @@ import { z } from 'zod';
 
 import { createTRPCRouter, publicProcedure } from '~/server/api/trpc';
 
-export const exampleRouter = createTRPCRouter({
+export const dogsRouter = createTRPCRouter({
   hello: publicProcedure.input(z.object({ text: z.string() })).query(({ input }) => {
     return {
       greeting: `Hello ${input.text}`,
     };
   }),
   getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
+    return ctx.prisma.dog.findMany();
   }),
 });
