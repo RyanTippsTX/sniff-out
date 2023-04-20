5a74f0a31d6ae900e1867a6db10e823e790f6a77
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2023-04-20 01:49:27 -0500

    revise layout of pup cards

--- src/pages/pups.tsx
+++ src/pages/pups.tsx
@@ -26,7 +26,7 @@ export default Home;
 
 function PupCard({ dog }: { dog: Dog }) {
   return (
-    <div key={dog.id} className='max-w-2xl overflow-clip rounded bg-white text-slate-800 sm:flex'>
+    <div key={dog.id} className='w-full max-w-xl rounded bg-white text-slate-800 sm:flex'>
       {/* <pre>{JSON.stringify(dog, null, 2)}</pre> */}
       <div className='mb-4 flex-shrink-0 sm:mb-0 sm:mr-4'>
         <svg
@@ -42,14 +42,10 @@ function PupCard({ dog }: { dog: Dog }) {
       </div>
       <div>
         <h4 className='text-lg font-bold'>{dog.name}</h4>
-        <p className='text-md'>{dog.size}</p>
+        <p className='text-md italic'>{dog.breed || 'unknown breed'}</p>
         <p className='text-md'>size: {dog.size}</p>
-        <p className='text-md'>breed: {dog.breed}</p>
         <p className='text-md'>owner: {dog.ownerId || 'n/a'}</p>
-        <p className='mt-1'>
-          Repudiandae sint consequuntur vel. Amet ut nobis explicabo numquam expedita quia omnis
-          voluptatem. Minus quidem ipsam quia iusto.
-        </p>
+        <p className='mt-1'>{dog.description}</p>
       </div>
     </div>
   );
